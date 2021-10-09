bea758ef6e9f9768a4851eef5668772f8d0ff5b4
lukehermann <hermann.s.luke@gmail.com>
2021-10-08 17:14:42 -0700

    Created models with some variables. league and settings are can be added to db

--- FFB_Website/models.py
+++ FFB_Website/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.db.models.aggregates import Max
 from django.db.models.deletion import CASCADE
+from django.db.models.fields import related
 from django.db.models.fields.related import OneToOneField
 
+
 # Create your models here.
-class League(models.Model):
+class ff_League(models.Model):
     league_id = models.IntegerField()
     year = models.IntegerField()
-    settings_id = OneToOneField(Settings, on_delete=models.CASCADE, primary_key=true)
+    current_week = models.IntegerField()
+    nfl_week = models.IntegerField()
 
 # league_id: int
 # year: int
@@ -16,17 +20,19 @@ class League(models.Model):
 # current_week: int # current fantasy football week
 # nfl_week: int # current nfl week
 
+
 class Settings(models.Model):
     reg_season_count = models.IntegerField()
     veto_votes_required = models.IntegerField()
     team_count = models.IntegerField()
     playoff_team_count = models.IntegerField()
     keeper_count = models.IntegerField()
-    trade_deadline = models.IntegerField()
+    # Trade Deadline epoch int wont convert correctly using dateTime
+    # trade_deadline = models.DateTimeField()
     name = models.CharField(max_length=64)
-    tie_rule = models.IntegerField()
-    playoff_seed_tie_rule = models.IntegerField()
-
+    tie_rule = models.IntegerField(null=True, blank=True)
+    playoff_seed_tie_rule = models.IntegerField(null=True, blank=True)
+    league = models.OneToOneField(ff_League, on_delete=models.CASCADE)
 
 #     reg_season_count: int
 # veto_votes_required: int
@@ -83,14 +89,62 @@ class Team(models.Model):
 
 
 
-
 class Matchup(models.Model):
-    home_team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team')
     home_score = models.IntegerField()
-    away_team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_team')
     away_score = models.IntegerField()
     
 # home_team: Team
 # home_score: int
 # away_team: Team
 # away_score: int
+
+class BoxScore(models.Model):
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='_home_team')
+    home_score = models.IntegerField()
+    home_projected = models.IntegerField()
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='_away_team')
+    away_score = models.IntegerField()
+    away_projected = models.IntegerField()
+    # home_lineup = 
+    # away_lineup = 
+
+# home_team: Team
+# home_score: int
+# home_projected: int # if it is not the current week this will be -1
+# away_team: Team
+# away_score: int
+# away_projected: int # if it is not the current week this will be -1
+# home_lineup: List[BoxPlayer]
+# away_lineup: List[BoxPlayer]
+
+class Player(models.Model):
+    name = models.CharField(max_length=256)
+    playerID = models.IntegerField()
+    posRank = models.IntegerField()
+    # eligibleSlots 
+    acquisitionType = models.CharField(max_length=64)
+    proTeam = models.CharField(max_length=6)
+    position = models.CharField(max_length=64)
+    injuryStatus = models.CharField(max_length=64)
+    injured = models.BooleanField()
+    total_points = models.IntegerField()
+    projected_total_points = models.IntegerField()
+    # stats = 
+
+
+
+# name: str
+# playerId: int
+# posRank: int # players positional rank
+# eligibleSlots: List[str] # example ['WR', 'WR/TE/RB']
+# acquisitionType: str
+# proTeam: str # 'PIT' or 'LAR'
+# position: str # main position like 'TE' or 'QB'
+# injuryStatus: str
+# injured: boolean
+# total_points: int # players total points during the season
+# projected_total_points: int # projected player points for the season
+# stats: dict # holds each week stats, actual and projected points. 
+# # week 1 example {1: {'breakdown': {'receivingReceptions': 1.7, 'rushingYards': 7.9}, 'points': 14.57, 'projected_points': 24.64, 'projected_breakdown': {'receivingReceptions': 6.354896833, 'rushingYards': 7.890957077}}}
--- FFB_Website/views.py
+++ FFB_Website/views.py
@@ -1,45 +1,118 @@
+import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db import models
+import time
 from espn_api.football import League
+
+from FFB_Website.models import Settings, ff_League
 
 
 # Create your views here.
 def home(request):
 
-    league = League(league_id=206814, year=2021, debug=True);
+
+    # Get current Year
+    league = League(league_id=206814, year=2021);
     standings = league.standings();
     standings_txt = [f"{pos + 1}: {team.team_name} ({team.wins}-{team.losses})" for \
         pos, team in enumerate(standings)];  
-
     scoreboard = get_scoreboard_short(league, 4);
-
     ranking = get_power_rankings(league);
-    return render(request, 'home.html', {'league': standings_txt, 'scoreboard': scoreboard, 'ranking': ranking})  # show the page with all the submissions
+    # print(league.settings.reg_season_count)
+
+    # Add instance of league to database if it doesn't currently exist
+    if(not ff_League.objects.filter(year = league.year).exists() and ff_League.objects.filter(league_id = league.league_id)):
+        # Add new league obj to database
+        new_league = ff_League(league_id = league.league_id, year = league.year, current_week = league.current_week, nfl_week = league.nfl_week)
+        new_league.save()
+
+        # Add the league's settings to database
+        new_settings = league.settings;
+        # print(datetime.datetime.fromtimestamp(new_settings.trade_deadline))
+        league_settings = Settings(reg_season_count = new_settings.reg_season_count, veto_votes_required = new_settings.veto_votes_required, team_count = new_settings.team_count, playoff_team_count = new_settings.playoff_team_count, keeper_count = new_settings.keeper_count, name = new_settings.name, tie_rule = (None if new_settings.tie_rule == None else 0), league = new_league)
+        league_settings.save();
+
+
+    return render(request, 'home.html', {'league': standings_txt, 'scoreboard': scoreboard, 'ranking': ranking});  # show the page with all the submissions
     # return HttpResponse("Hello, Django!")
 
 
 
 def get_scoreboard_short(league, week=None):
     #Gets current week's scoreboard
-    box_scores = league.box_scores(week=week)
+    box_scores = league.box_scores(week=week);
     score = ['%s %.2f - %.2f %s' % (i.home_team.team_abbrev, i.home_score,
                 i.away_score, i.away_team.team_abbrev) for i in box_scores
-                if i.away_team]
-    text = ['Score Update'] + score
-    return '\n'.join(text)
+                if i.away_team];
+    text = ['Score Update'] + score;
+    return '\n'.join(text);
 
 
 def get_power_rankings(league, week=None):
     # power rankings requires an integer value, so this grabs the current week for that
     if not week:
-        week = league.current_week
+        week = league.current_week;
     #Gets current week's power rankings
     #Using 2 step dominance, as well as a combination of points scored and margin of victory.
     #It's weighted 80/15/5 respectively
-    power_rankings = league.power_rankings(week=week)
+    power_rankings = league.power_rankings(week=week);
 
     score = ['%s - %s' % (i[0], i[1].team_name) for i in power_rankings
-                if i]
-    text = ['Power Rankings'] + score
+                if i];
+    text = ['Power Rankings'] + score;
+    return '\n'.join(text);
+
+
+def get_trophies(league, week=None):
+    #Gets trophies for highest score, lowest score, closest score, and biggest win
+    matchups = league.box_scores(week=week)
+    low_score = 9999
+    low_team_name = ''
+    high_score = -1
+    high_team_name = ''
+    closest_score = 9999
+    close_winner = ''
+    close_loser = ''
+    biggest_blowout = -1
+    blown_out_team_name = ''
+    ownerer_team_name = ''
+
+    for i in matchups:
+        if i.home_score > high_score:
+            high_score = i.home_score
+            high_team_name = i.home_team.team_name
+        if i.home_score < low_score:
+            low_score = i.home_score
+            low_team_name = i.home_team.team_name
+        if i.away_score > high_score:
+            high_score = i.away_score
+            high_team_name = i.away_team.team_name
+        if i.away_score < low_score:
+            low_score = i.away_score
+            low_team_name = i.away_team.team_name
+        if i.away_score - i.home_score != 0 and \
+            abs(i.away_score - i.home_score) < closest_score:
+            closest_score = abs(i.away_score - i.home_score)
+            if i.away_score - i.home_score < 0:
+                close_winner = i.home_team.team_name
+                close_loser = i.away_team.team_name
+            else:
+                close_winner = i.away_team.team_name
+                close_loser = i.home_team.team_name
+        if abs(i.away_score - i.home_score) > biggest_blowout:
+            biggest_blowout = abs(i.away_score - i.home_score)
+            if i.away_score - i.home_score < 0:
+                ownerer_team_name = i.home_team.team_name
+                blown_out_team_name = i.away_team.team_name
+            else:
+                ownerer_team_name = i.away_team.team_name
+                blown_out_team_name = i.home_team.team_name
+
+    low_score_str = ['Low score: %s with %.2f points' % (low_team_name, low_score)]
+    high_score_str = ['High score: %s with %.2f points' % (high_team_name, high_score)]
+    close_score_str = ['%s barely beat %s by a margin of %.2f' % (close_winner, close_loser, closest_score)]
+    blowout_str = ['%s blown out by %s by a margin of %.2f' % (blown_out_team_name, ownerer_team_name, biggest_blowout)]
+
+    text = ['Trophies of the week:'] + low_score_str + high_score_str + close_score_str + blowout_str
     return '\n'.join(text)
